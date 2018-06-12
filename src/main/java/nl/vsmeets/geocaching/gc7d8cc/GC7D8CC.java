16a5a1a3697b6bc486320dd0ed17313e685c3473
Vincent Smeets <Vincent.VSmeets@GMail.com>
2018-06-12 20:06:40 +0200

    Added the main class

---
+++ src/main/java/nl/vsmeets/geocaching/gc7d8cc/GC7D8CC.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package nl.vsmeets.geocaching.gc7d8cc;
+
+/**
+ * Solve the mystery GC7D8CC.
+ *
+ * @author vincent
+ */
+public class GC7D8CC {
+
+	/**
+	 * The main entry point.
+	 *
+	 * @param args
+	 *            The command line arguments.
+	 */
+	public static void main(final String[] args) {
+	}
+
+}
